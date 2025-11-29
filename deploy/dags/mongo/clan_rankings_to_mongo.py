32631536539a6a8b2f6c139c98ad58dd6de1b275
EgorChernov5 <krish19poroh@mail.ru>
2025-11-29 07:12:44 +0300

    Add DAGs

--- deploy/dags/mongo/clan_rankings_to_mongo.py
+++ deploy/dags/mongo/clan_rankings_to_mongo.py
@@ -3,8 +3,22 @@ from datetime import datetime
 
 from airflow.operators.python import PythonOperator
 
-from operators.mongo_insert_operator import MongoInsertOperator
+# from operators.mongo_insert_operator import MongoInsertOperator
+from hooks.mongo_hook import MongoHook
 from utils.clash_of_clans_api import get_clan_rankings
+
+
+def insert_clan_rankings_to_mongo(**context):
+    # Получаем данные из XCom
+    data = context['ti'].xcom_pull(task_ids='get_clan_rankings')
+    
+    if not data:
+        raise ValueError("No data received from get_clan_rankings")
+    
+    hook = MongoHook()
+    inserted_ids = hook.insert("clan_rankings", data)
+    print(f"Inserted documents: {inserted_ids}")
+    return inserted_ids
 
 
 with DAG(
@@ -19,11 +33,11 @@ with DAG(
         python_callable=get_clan_rankings,
         provide_context=True
     )
-    
-    insert_task = MongoInsertOperator(
-        task_id="insert_data",
-        collection="clan_rankings",
-        documents="{{ ti.xcom_pull(task_ids='get_clan_rankings') }}"
+
+    insert_task = PythonOperator(
+        task_id="insert_clan_rankings",
+        python_callable=insert_clan_rankings_to_mongo,
+        provide_context=True
     )
 
     fetch_task >> insert_task
