ebf6318950ddf850cdca6a4e67ec443d4023262f
Artyom Chebykin <chebart03@gmail.com>
2025-12-11 15:03:41 +0300

    adedded postgres hook

--- plugins/hooks/__init__.py
+++ plugins/hooks/__init__.py
@@ -1,3 +1,4 @@
 from plugins.hooks.airflow_api_hook import AirflowAPIHook
 from plugins.hooks.minio_hook import MinioHook
 from plugins.hooks.mongodb_hook import MongoDBHook
+from plugins.hooks.postgres_hook import PostgresDataHook
