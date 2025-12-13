5848d8875f2ba2746641867062ea63e34aec311c
EgorChernov5 <krish19poroh@mail.ru>
2025-12-13 04:34:24 +0300

    Add minio tasks

--- plugins/utils/minio_tasks.py
+++ plugins/utils/minio_tasks.py
@@ -9,6 +9,7 @@ import hashlib
 import pandas as pd
 
 from plugins.hooks import MinioHook
+from plugins.utils import tools
 
 # ------------------
 # tasks before minio
@@ -143,61 +144,107 @@ def postprocess_minio_raw_data(**context):
 # ------------------
 
 def split_minio_raw_data(**context):
-    # get data from minio
+    # Get data from minio
     raw_clans_dict = context["ti"].xcom_pull(task_ids="load_minio_raw_clan_data")
     raw_players_dict = context["ti"].xcom_pull(task_ids="load_minio_raw_data")
 
     # Clans info
+    clans = {}                  # tag, name, members_count, war_wins_count, clan_level, clan_points
     if raw_clans_dict is not None:
         for clan_tag, raw_df in raw_clans_dict.items():
-            # Table clan
-            tag = clan_tag
-            name = raw_df['name']
-            members_count = raw_df['members']
-            war_wins_count = raw_df['warWins']
-            clan_level = raw_df['clanLevel']
-            clan_points = raw_df['clanPoints']
-            break
+            for _, clan in raw_df.iterrows():
+                # Table clan
+                clans[clan_tag] = {
+                    'name': clan['name'],
+                    'members_count': clan['members'],
+                    'war_wins_count': clan['warWins'],
+                    'clan_level': clan['clanLevel'],
+                    'clan_points': clan['clanPoints']
+                }
 
     # Players info
+    players = {}                # tag, tag_clan, name, town_hall_level, id_league
+    leagues = {}                # id_league, league_name
+    achievements = {}           # id_achievement, name, max_starts=3
+    player_achievements = {}    # tag, id_achievement, stars
+    player_camps = {}           # tag, id_item, level, icon_link
+    items = {}                  # id_item, name, item_type, village, max_level
     if raw_players_dict is not None:
-        achievements = {}  # id, name
-        player_achievements = {}
         for clan_tag, raw_df in raw_players_dict.items():
-            print(raw_df.columns)
             for _, player in raw_df.iterrows():
                 # Table player
                 tag = player['tag']
-                tag_clan = clan_tag
-                name = player['name']
-                town_hall_level = player['townHallLevel']
-                id_league = player['league']
-                
-                # Table player_achievement
-                player_achievements[tag] = {
-                    'id_achievement': [],
-                    'progress': []
+                id_league = player['league']['id'] if player['league'] is not None else None  # TODO: is it ok?
+
+                players[tag] = {
+                    'tag_clan': clan_tag,
+                    'name': player['name'],
+                    'town_hall_level': player['townHallLevel'],
+                    'id_league': id_league
                 }
-                for achievement in player['achievements']:
-                    achievement_name = achievement['name']
-                    id_achievement = hashlib.sha256(str(achievement_name).encode()).hexdigest()
-                    progress = achievement['value']  # это progress?
-                    
-                    player_achievements[tag]['id_achievement'].append(id_achievement)
-                    player_achievements[tag]['progress'].append(progress)
-                    # Table Achievement
-                    achievements[id_achievement] = achievement_name
-
-                # Table player_camp
-
-                # Table item
 
                 # Table league
+                if id_league is not None: leagues[id_league] = player['league']['name']
+                
+                # Table player_achievement, achievement
+                tools.parse_achievements(player, player_achievements, achievements)
 
-                break
-            break
-        
+                # TODO: add icon_link
+                # Table player_camp, item (troops, heroes, spells)
+                tools.parse_items(player, 'troops', items, player_camps)
+                tools.parse_items(player, 'heroes', items, player_camps)
+                tools.parse_items(player, 'spells', items, player_camps)
 
+    return clans, players, leagues, achievements, player_achievements, player_camps, items
 
 def norm_minio_raw_data(**context):
-    pass
+    clans, players, leagues, achievements, player_achievements, player_camps, items = context["ti"].xcom_pull(task_ids="split_minio_raw_data")
+    
+    # Norm clans
+    norm_clans = [('tag', 'name', 'members_count', 'war_wins_count', 'clan_level', 'clan_points')]
+    for tag, data in clans.items():
+        # tag, name, members_count, war_wins_count, clan_level, clan_points
+        norm_clans.append((tag, data['name'], data['members_count'], data['war_wins_count'], data['clan_level'], data['clan_points']))
+    
+    # Norm players
+    norm_players = [('tag', 'tag_clan', 'name', 'town_hall_level', 'id_league')]
+    for tag, data in players.items():
+        # tag, tag_clan, name, town_hall_level, id_league
+        norm_players.append((tag, data['tag_clan'], data['name'], data['town_hall_level'], data['id_league']))
+    
+    # Norm leagues
+    # id_leagues, league_name
+    norm_leagues = [('id_leagues', 'league_name')]
+    for id_league, name in leagues.items():
+        norm_leagues.append((id_league, name))
+    
+    # Norm achievements
+    norm_achievements = [('id_achievement', 'name', 'max_starts')]
+    for id_achievement, data in achievements.items():
+        # id_achievement, name, max_starts
+        norm_achievements.append((id_achievement, data['name'], data['max_starts']))
+    
+    # Norm player_achievements
+    norm_player_achievements = [('tag', 'id_achievement', 'stars')]
+    for tag, data in player_achievements.items():
+        # tag, id_achievement, stars
+        for id_achievement, star in zip(data['id_achievement'], data['stars']):
+            norm_player_achievements.append((tag, id_achievement, star))
+    
+    # TODO: add icon_link
+    # Norm player_camps
+    # norm_player_camps = [('tag', 'id_item', 'level', 'icon_link')]
+    norm_player_camps = [('tag', 'id_item', 'level')]
+    for tag, data in player_camps.items():
+        # tag, id_item, level, icon_link
+        # for id_item, level, icon_link in zip(data['id_item'], data['level'], data['icon_link']):
+        for id_item, level in zip(data['id_item'], data['level']):
+            norm_player_camps.append((tag, id_item, level))
+    
+    # Norm items
+    norm_items = [('id_item', 'name', 'item_type', 'village', 'max_level')]
+    for id_item, data in items.items():
+        # id_item, name, item_type, village, max_level
+        norm_items.append((id_item, data['name'], data['item_type'], data['village'], data['max_level']))
+
+    return norm_clans, norm_players, norm_leagues, norm_achievements, norm_player_achievements, norm_player_camps, norm_items
